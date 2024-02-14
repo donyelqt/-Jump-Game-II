5071cfe77bd0dfd553987d6f0cd0e6ca5cc17cb5
Doniele Arys Antonio <143719207+donyelqt@users.noreply.github.com>
2024-02-14 19:47:04 +0800

    Create Solution.cs

---
+++ Solution.cs
@@ -0,0 +1,27 @@
+public class Solution {
+    public int Jump(int[] nums) {
+        int n = nums.Length;
+        if (n <= 1) {
+            return 0;
+        }
+
+        int jumps = 0;
+        int maxReach = 0;
+        int currEnd = 0;
+
+        for (int i = 0; i < n - 1; i++) {
+            maxReach = Math.Max(maxReach, i + nums[i]);
+
+            if (i == currEnd) {
+                jumps++;
+                currEnd = maxReach;
+
+                if (currEnd >= n - 1) {
+                    break;
+                }
+            }
+        }
+
+        return jumps;
+    }
+}
